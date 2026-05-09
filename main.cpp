4c0d2600421758e8298a1d7f14b956469e9cb325
saboyaa <112286103+Saboyaa@users.noreply.github.com>
2026-05-09 19:45:08 -0300

    final image

--- main.cpp
+++ main.cpp
@@ -54,8 +54,8 @@ int main() {
 
     cam.aspect_ratio      = 16.0 / 9.0;
     cam.img_width       = 1200;
-    cam.samples_per_pixel = 10;
-    cam.max_depth         = 10;
+    cam.samples_per_pixel = 500;
+    cam.max_depth         = 50;
 
     cam.vfov     = 20;
     cam.lookfrom = point3(13,2,3);
